6ca6e973f20b3a5b680c342e2aa1994c2acefa30
Rahul <rahulpatnaiknb@gmail.com>
2026-01-14 20:34:56 +0530

    Middle Practices.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 console.log("Starting point");
 const express = require('express');
 const app = express();
+const {adminAuth} = require('./middlewares/auth');
 
-app.use("/get", (req, res, next) => {
-    res.send("Started from the server.")
+// app.use("/get", (req, res, next) => {
+//     res.send("Started from the server.")
+// });
+
+app.use("/admin",adminAuth)
+
+app.post("/user/login", (req, res)=> {
+    res.send("User logged in.");
+})
+app.get("/admin/get", (req, res, next) => {
+    // res.send("Started from the server.");
+    next();
+},
+(req, res, next) => {
+    res.send("Started from the server2.");
+}
+);
+
+
+app.delete("/hello", (req, res, next) => {    
+    res.send("Hello Hello Hello.")
 });
 
 app.listen(3000, () => {
